3ef03fd84ead27fe0ab315bc1c71cd4bb85db32c
MichaelZ1999 <98940846+MichaelZ1999@users.noreply.github.com>
2023-08-22 00:23:39 +0300

    progress bar working

--- src/components/google-drive/AddFileButton.js
+++ src/components/google-drive/AddFileButton.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState } from "react"
+import ReactDOM from "react-dom"
 import { faFileUpload } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { useAuth } from '../../contexts/AuthContext'
-import { storage, database } from '../../firebase'
-import { ROOT_FOLDER } from '../../hooks/useFolder'
+import { useAuth } from "../../contexts/AuthContext"
+import { storage, database } from "../../firebase"
+import { ROOT_FOLDER } from "../../hooks/useFolder"
+import { ProgressBar, Toast } from "react-bootstrap"
+import { v4 as uuidv4} from 'uuid'
 
 export default function AddFileButton({ currentFolder }) {
+    const [uploadingFiles, setUploadingFiles] = useState([])
     const { currentUser } = useAuth()
 
     function handleUpload(e) {
         const file = e.target.files[0]
         if (currentFolder == null || file == null) return
+
+        const id = uuidv4()
+        setUploadingFiles(prevUploadingFiles => [
+            ...prevUploadingFiles,
+            { id: id, name: file.name, progress: 0, error: false },
+        ])
 
         const filePath =
             currentFolder === ROOT_FOLDER
@@ -22,10 +32,25 @@ export default function AddFileButton({ currentFolder }) {
             .put(file)
 
         uploadTask.on( "state_changed", snapshot => {
-
+            const progress = snapshot.bytesTransferred / snapshot.totalBytes
+            setUploadingFiles(prevUploadingFiles => {
+                return prevUploadingFiles.map(uploadFile => {
+                    if (uploadFile.id === id)
+                        return { ...uploadFile, progress: progress 
+                    }
+                    return uploadFile
+                })
+            })
         },() => {   
 
         },() => {
+            setUploadingFiles(prevUploadingFiles => {
+                return prevUploadingFiles.filter(uploadFile => {
+                    return uploadFile.id !== id
+                })
+
+            } )
+            
             uploadTask.snapshot.ref.getDownloadURL().then(url => {
                 database.files.add({
                     url: url,
@@ -49,6 +74,39 @@ export default function AddFileButton({ currentFolder }) {
                     style={{ opacity: 0, position: "absolute", left: "-9999px" }}
                 />
             </label>
+            {uploadingFiles.length > 0 &&
+                ReactDOM.createPortal(
+                <div
+                style={{
+                  position: "absolute",
+                  bottom: "1rem",
+                  right: "1rem",
+                  maxWidth: "250px"
+                }}>
+                 {uploadingFiles.map(file => (
+                    <Toast key={file.id}>
+                        <Toast.Header 
+                            closeButton={file.error}
+                            className='text-truncate w-100 d-block'
+                        >
+                            {file.name}
+                        </Toast.Header>
+                        <Toast.Body>
+                            <ProgressBar
+                                animated={!file.error}
+                                variant={file.error ? 'danger' : 'primary'}
+                                now={file.error ? 100 : file.progress * 100 }
+                                label={
+                                    file.error ? "Error" : 
+                                    `${Math.round(file.progress * 100)}%`
+                                } 
+                            />
+                        </Toast.Body>
+                    </Toast>
+                 ))}
+                </div>, 
+                document.body
+            )}
         </>
   )
 }
